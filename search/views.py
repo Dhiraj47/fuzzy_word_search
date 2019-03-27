06690730abca0e20915e070cb6fe0678cf3899d9
Dhiraj47 <44241277+Dhiraj47@users.noreply.github.com>
2019-03-27 11:42:40 +0530

    Update views.py

--- search/views.py
+++ search/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from json import load
 from fuzzywuzzy.process import extract
 from django.http import JsonResponse
-
+from os import getcwd
+import re
 # Create your views here.-------------------------------------------------------
-with open('words_dictionary.json', 'r') as f:
+with open(getcwd()+'/fuzzy_word_search/words_dictionary.json', 'r') as f:
 	distros_dict = load(f)
 f.close()
 
@@ -12,13 +13,23 @@ f.close()
 def index(req):
 	return render(req, 'index.html')
 
+def fuzzyfinder(user_input, collection):
+    suggestions = []
+    pattern = '.*?'.join(user_input)   # Converts 'djm' to 'd.*?j.*?m'
+    regex = re.compile(pattern)  # Compiles a regex.
+    for item in collection:
+        match = regex.search(item)   # Checks if the current item matches the regex.
+        if match:
+            suggestions.append((len(match.group()), match.start(), item))
+    return [x for _, _, x in sorted(suggestions)]
 
 def search(req):
 	global distros_dict
-	search_word = req.GET['keyword']
-	a = [key for key, value in distros_dict.items() if search_word in key.lower()]
-	
+	search_word = req.GET['keyword'].lower()
+	a = [key for key, value in distros_dict.items() if search_word in key]
+	a = fuzzyfinder(search_word, a)
+
 	result = dict(extract(search_word, a, limit=25))
 	sortedlist = list(dict(sorted(result.items(), key=lambda s: len(s[0]))).keys())
-	
+
 	return JsonResponse({'result': sortedlist})
